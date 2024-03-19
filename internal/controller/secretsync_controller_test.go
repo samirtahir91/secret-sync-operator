8226f0a2957abfa4a474298acae4f8ef67bab755
samir-tahir <samirtahir91@gmail.com>
2024-03-19 16:25:44 +0000

    tests

--- internal/controller/secretsync_controller_test.go
+++ internal/controller/secretsync_controller_test.go
@@ -25,7 +25,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	//"k8s.io/apimachinery/pkg/types"
-	syncv1 "secret-sync-operator/api/v1"
+	//syncv1 "secret-sync-operator/api/v1"
 )
 
 var _ = Describe("SecretSync controller", func() {
