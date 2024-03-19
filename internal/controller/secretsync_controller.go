b6c5b3379df437fc5e7f0fabb4b544bfc89b0e79
samir-tahir <samirtahir91@gmail.com>
2024-03-19 16:02:59 +0000

    tests

--- internal/controller/secretsync_controller.go
+++ internal/controller/secretsync_controller.go
@@ -41,18 +41,12 @@ type SecretSyncReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+// RBAC
 //+kubebuilder:rbac:groups=sync.samir.io,resources=secretsyncs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;update;create;delete;watch;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the SecretSync object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
 func (r *SecretSyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	l := log.FromContext(ctx)
 	l.Info("Enter Reconcile", "req", req)
@@ -149,6 +143,7 @@ func (r *SecretSyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	return ctrl.Result{}, nil
 }
 
+// Delete unreferenced secrets owned by the SecretSync object
 func (r *SecretSyncReconciler) deleteUnreferencedSecrets(ctx context.Context, secretSync *syncv1.SecretSync) error {
 	l := log.FromContext(ctx)
 	
--- internal/controller/secretsync_controller_test.go
+++ internal/controller/secretsync_controller_test.go
@@ -1,127 +1,82 @@
-package controller_test
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
 
 import (
 	"context"
-	"errors"
-	"os"
-	"path/filepath"
-	"runtime"
-	"testing"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 
-	syncv1 "secret-sync-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes/scheme"
-    "k8s.io/client-go/rest"
-
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	tutorialv1 "my.domain/tutorial/api/v1"
 )
 
-var (
-	testEnv   *envtest.Environment
-	cfg       *rest.Config
-	k8sClient client.Client
-	ctx       context.Context
-	cancel    context.CancelFunc
-)
+var _ = Describe("SecretSync controller", func() {
 
-func TestControllers(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Controller Suite")
-}
+	const (
+		secretSyncName1 = "secret-sync-1"
+		secret1 = "secret-1"
 
-var _ = BeforeSuite(func() {
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		ErrorIfCRDPathMissing: true,
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-	}
 
-	var err error
-	cfg, err = testEnv.Start()
-	Expect(err).ToNot(HaveOccurred())
-	Expect(cfg).ToNot(BeNil())
+		secretSyncName2   = "secret-sync-2"
+		secret2 = "secret-2"
 
-	ctx, cancel = context.WithCancel(context.Background())
+		sourceNamespace = "default"
+        destinationNamespace = "foo"
+	)
 
-    k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
-        Scheme: scheme.Scheme,
-    })
-    
-	Expect(err).ToNot(HaveOccurred())
-
-	reconciler := &controllers.SecretSyncReconciler{
-		Client: k8sManager.GetClient(),
-		Scheme: k8sManager.GetScheme(),
-	}
-	err = reconciler.SetupWithManager(k8sManager)
-	Expect(err).ToNot(HaveOccurred())
-
-	k8sClient = k8sManager.GetClient()
-
-	go func() {
-		defer GinkgoRecover()
-		err := k8sManager.Start(ctx)
-		Expect(err).ToNot(HaveOccurred())
-	}()
-})
-
-var _ = AfterSuite(func() {
-	cancel()
-	err := testEnv.Stop()
-	Expect(err).ToNot(HaveOccurred())
-})
-
-var _ = Describe("Controller", func() {
-	Context("When reconciling SecretSync", func() {
-		It("should reconcile secrets", func() {
-			// Mock a SecretSync object
-			secretSync := &syncv1.SecretSync{
-				ObjectMeta: metav1.ObjectMeta{Name: "test-secretsync"},
-				Spec: syncv1.SecretSyncSpec{
-					Secrets: []string{"secret1", "secret2"},
+	Context("When setting up the test environment", func() {
+		It("Should create SecretSync custom resources", func() {
+			By("Creating the secret")
+			ctx := context.Background()
+            secret := corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      secret1,
+					Namespace: sourceNamespace,
 				},
 			}
+			Expect(k8sClient.Create(ctx, &secret)).Should(Succeed())
 
-			// Create the SecretSync object
-			err := k8sClient.Create(ctx, secretSync)
-			Expect(err).ToNot(HaveOccurred())
-
-			// Mock a source secret
-			sourceSecret := &corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{Name: "secret1", Namespace: "default"},
-				Data:       map[string][]byte{"key": []byte("value")},
+			By("Creating a first SecretSync custom resource")
+			secretSync1 := tutorialv1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      secretSyncName1,
+					Namespace: destinationNamespace,
+				},
+				Spec: tutorialv1.SecretSyncSpec{
+					secrets: [secret1],
+				},
 			}
+			Expect(k8sClient.Create(ctx, &secretSync1)).Should(Succeed())
 
-			// Create the source secret
-			err = k8sClient.Create(ctx, sourceSecret)
-			Expect(err).ToNot(HaveOccurred())
-
-			// Wait for reconciliation
-			Eventually(func() error {
-				err := k8sClient.Get(ctx, client.ObjectKey{Name: "secret1", Namespace: "default"}, &corev1.Secret{})
-				return err
-			}).Should(Succeed())
-
-			// Verify the destination secret is created
-			destinationSecret := &corev1.Secret{}
-			err = k8sClient.Get(ctx, client.ObjectKey{Name: "secret1", Namespace: "test"}, destinationSecret)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(destinationSecret.Data).To(Equal(sourceSecret.Data))
-
-			// Clean up
-			err = k8sClient.Delete(ctx, secretSync)
-			Expect(err).ToNot(HaveOccurred())
-			err = k8sClient.Delete(ctx, sourceSecret)
-			Expect(err).ToNot(HaveOccurred())
-			err = k8sClient.Delete(ctx, destinationSecret)
-			Expect(err).ToNot(HaveOccurred())
+			By("Creating another SecretSync custom resource")
+			secretSync2 := tutorialv1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      secretSyncName2,
+					Namespace: destinationNamespace,
+				},
+				Spec: tutorialv1.SecretSyncSpec{
+					Name: [secret2],
+				},
+			}
+			Expect(k8sClient.Create(ctx, &secretSync2)).Should(Succeed())
 		})
 	})
 })
