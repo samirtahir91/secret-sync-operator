0d3ce8e23da2873b93201961fbab92b75957de7a
samir-tahir <samirtahir91@gmail.com>
2024-03-20 12:34:09 +0000

    test

--- internal/controller/secretsync_controller.go
+++ internal/controller/secretsync_controller.go
@@ -23,14 +23,17 @@ import (
 
 	//apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	    "sigs.k8s.io/controller-runtime/pkg/client"
+
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	//"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	corev1 "k8s.io/api/core/v1"
-	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	syncv1 "secret-sync-operator/api/v1"
 
