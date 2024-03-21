22187fe6df6419c3186f0db236820e91160aa58b
samir-tahir <samirtahir91@gmail.com>
2024-03-21 13:49:54 +0000

    refactor

--- internal/controller/secretsync_controller.go
+++ internal/controller/secretsync_controller.go
@@ -35,9 +35,9 @@ import (
 
 	syncv1 "secret-sync-operator/api/v1"
 
-    "sigs.k8s.io/controller-runtime/pkg/builder" // Required for Watching
+    //"sigs.k8s.io/controller-runtime/pkg/builder" // Required for Watching
     "sigs.k8s.io/controller-runtime/pkg/handler" // Required for Watching
-    "sigs.k8s.io/controller-runtime/pkg/predicate" // Required for Watching
+    //"sigs.k8s.io/controller-runtime/pkg/predicate" // Required for Watching
     "sigs.k8s.io/controller-runtime/pkg/reconcile" // Required for Watching
     "sigs.k8s.io/controller-runtime/pkg/source" // Required for Watching
 )
